4ef1a211e7a333658f92bc34c6d75aa627579162
Wudrus <woody.play.pixbox@gmail.com>
2026-01-31 23:50:17 +0300

    Quartz sync: Jan 31, 2026, 11:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "tGIoS Wiki",
+    pageTitle: "Grand Hazard Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
